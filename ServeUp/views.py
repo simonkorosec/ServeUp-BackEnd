c4b4fd4da5c80aaab098fb1d2e9012d6a5906a45
jrepe <repe.jure@gmail.com>
2018-11-16 09:50:02 +0100

    Added basic API endpoints for dummy User and Group

--- ServeUp/views.py
+++ ServeUp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User, Group
+from ServeUp.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
